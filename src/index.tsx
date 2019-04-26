9d4d2e8facd5e477485f6db39935c736bfba9269
Gong Lixian <gong12339@hotmail.com>
2019-04-26 16:27:57 +0800

    :hammer: remove all files

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Hello } from '@components/hello';
-
-ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
-  document.getElementById('root')
-);
